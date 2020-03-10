26447916522a2e1af492b29ebcaa6c8aff919c25
Rafal <strozyk.rafal@gmail.com>
2020-03-10 23:13:19 +0100

    Commit on master

---
+++ #7 stringy_strings.py
@@ -0,0 +1,15 @@
+# write me a function stringy that takes a size and returns a string of alternating '1s' and '0s'.
+# the string should start with a 1.
+# a string with size 6 should return :'101010'.
+# with size 4 should return : '1010'.
+# with size 12 should return : '101010101010'.
+# The size will always be positive and will only use whole numbers.
+
+def stringy(size):
+    new_string = ""     # defining new string
+    new_string += int(size/2)*"10"  # divide size by 2 and adding that many '10's
+    if size % 2 != 0:               # if size will be odd number we need to add 1 at the end of new_string
+        new_string += "1"
+    return new_string
+
+print(stringy(5))   # test
